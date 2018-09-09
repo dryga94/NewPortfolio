8f23d50e58dca7e049e22133e0bf9bfc26914e28
dryga94 <dryga94@gmail.com>
2018-09-09 21:30:34 +0300

    finish singlework page

--- src/js/modules/init.js
+++ src/js/modules/init.js
@@ -7,4 +7,21 @@ document.addEventListener("DOMContentLoaded", function (event){
         $('.scrollbar-outer').scrollbar();
         console.log('ready');
     }
+
+    var galleryTop = new Swiper('.gallery-top', {
+        slidesPerView: 1,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+      var galleryThumbs = new Swiper('.gallery-thumbs', {
+        spaceBetween: 10,
+        centeredSlides: true,
+        slidesPerView: 4,
+        touchRatio: 0.2,
+        slideToClickedSlide: true,
+      });
+      galleryTop.controller.control = galleryThumbs;
+      galleryThumbs.controller.control = galleryTop;
 });
--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
 //@prepros-append ./modules/slidedown.js
 //@prepros-append ./modules/navigation.js
 //@prepros-append ./modules/init.js
+//@prepros-append ./modules/swiper.js
