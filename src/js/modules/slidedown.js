228048d9f89ff970452fa191ac2da2282cdb2b66
dryga94 <dryga94@gmail.com>
2018-09-16 18:52:18 +0300

    try to fix slidedown script + add singlePost page

--- src/js/modules/slidedown.js
+++ src/js/modules/slidedown.js
@@ -1,12 +1,16 @@
 $('.js-dropdown').on('click', function (){
-    $('.js-dropdown__body').slideToggle();
+    $(this).parent().find('.js-dropdown__body').slideToggle();
+    console.log($('.js-dropdown').find('.fas'))
 })
 $(document).mouseup(function (close){ 
     var div = $(".js-dropdown__body"); 
     if (!div.is(close.target)
-        && !$('.js-dropdown').is(close.target) 
-        && div.has(close.target).length === 0) { 
+        && !$('.js-dropdown').is(close.target)
+        // && !$('.js-dropdown').find('.fas').is(close.target) 
+        && div.has(close.target).length === 0) 
+        { 
         div.slideUp(); 
+        
     }
 });
 
